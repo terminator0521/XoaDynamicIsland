3c5250a13a9f6653ca44aab3b29fa68657af44c6
Ricky Xu <89417286+terminator0521@users.noreply.github.com>
2026-08-31 03:43:53 +0800

    fix time not properly syncing with windows time

--- XoaUIElements/UIElements/Clock.cpp
+++ XoaUIElements/UIElements/Clock.cpp
@@ -1,18 +1,33 @@
 #include "Clock.hpp"
 #include <raylib.h>
 #include <format>
-#include <chrono>
 #include <iostream>
+#include <ctime>
 
 Clock::Clock(const Rectangle box) : UIElement(box) , fontSize(30) {}
 
 void Clock::update()
 {
-    auto now = std::chrono::system_clock::now();
+    auto now = std::time(nullptr);
+
+    std::tm localTime{};
+    localtime_s(&localTime, &now);
+
     //get string ver of current time
-    currentTime = std::format("{:%H:%M}", now + std::chrono::hours(-4));
-    currentDate = std::format("{:%m-%d-%Y}", now + std::chrono::hours(-4));
-    currentDay = std::format("{:%A}", now + std::chrono::hours(-4));
+    currentTime = std::format("{:02}:{:02}",
+            localTime.tm_hour,
+            localTime.tm_min);
+
+    currentDate = std::format("{:02}-{:02}-{:04}",
+        localTime.tm_mon + 1,
+        localTime.tm_mday,
+        localTime.tm_year + 1900);
+
+    char day[20];
+    std::strftime(day, sizeof(day), "%A", &localTime);
+
+    currentDay = day;
+
 }
 
 void Clock::render()
